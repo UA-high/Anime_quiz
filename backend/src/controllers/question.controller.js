562a58da01e75e1d133ba6a1532f0e2aaf6db7ae
adityapaul26 <adityapaul2603@gmail.com>
2026-04-10 20:50:36 +0530

    feat: options add to mcq

--- backend/src/controllers/question.controller.js
+++ backend/src/controllers/question.controller.js
@@ -1,110 +1,101 @@
-const mcq = require("../models/mcq.model")
-const picqsModel = require("../models/picqs.model")
-const jwt = require('jsonwebtoken')
-const config = require("../config/config")
-const uploadToCloud = require("../services/uploadImage.service")
-const mcqModel = require("../models/mcq.model")
-
+const mcq = require("../models/mcq.model");
+const picqsModel = require("../models/picqs.model");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+const uploadToCloud = require("../services/uploadImage.service");
+const mcqModel = require("../models/mcq.model");
 
 // Function to upload MCQs
-async function uploadMcqQuestions(req, res){
-    const token = req.cookies.token
+async function uploadMcqQuestions(req, res) {
+  const token = req.cookies.token;
 
-    if(!token){
-        return res.status(401).json({
-            messege:"Unauthorized"
-        })
+  if (!token) {
+    return res.status(401).json({
+      messege: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+
+    if (decoded.role !== "admin") {
+      return res.status(401).json({
+        messege: "You are not allowed to upload questions",
+      });
     }
 
-    try{
-        const decoded = jwt.verify(
-            token,
-            config.JWT_SECRET
-        )
+    const { title, desc, opts } = req.body;
 
-        if(decoded.role !== 'admin'){
-            return res.status(401).json({
-                messege:"You are not allowed to upload questions"
-            })
-        }
+    let result = undefined;
 
-        const {title, desc} = req.body
-        
-        let result = undefined
-
-        if(req.file){
-            const file = req.file
-            //Upload the image to the image kit cloud
-            result = await uploadToCloud(file.buffer)
-        }
-
-        //Upload the question to database
-        const question = await mcqModel.create({
-            title,
-            desc,
-            image: result.url
-        })
-
-        res.status(200).json({
-            messege:"Question uploaded successfully",
-            question
-        })
-
-
-    }catch(err){
-        console.log(err)
-        return res.status(401).json({
-            messege:"Unauthorized",
-        })
+    if (req.file) {
+      const file = req.file;
+      //Upload the image to the image kit cloud
+      result = await uploadToCloud(file.buffer);
     }
+
+    //Upload the question to database
+    const question = await mcqModel.create({
+      title,
+      desc,
+      opts,
+      image: result ? result.url : "",
+    });
+
+    res.status(200).json({
+      messege: "Question uploaded successfully",
+      question,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({
+      messege: "Unauthorized",
+    });
+  }
 }
 
 // Upload picture questions
-async function uploadPictureQuestions(req, res){
-    const token = req.cookies.token
+async function uploadPictureQuestions(req, res) {
+  const token = req.cookies.token;
 
-    if(!token){
-        return res.status(401).json({
-            messege:"Unauthorized"
-        })
+  if (!token) {
+    return res.status(401).json({
+      messege: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+
+    if (decoded.role !== "admin") {
+      return res.status(401).json({
+        messege: "You are not allowed to upload questions",
+      });
     }
 
-    try{
-        const decoded = jwt.verify(
-            token,
-            config.JWT_SECRET
-        )
+    const file = req.file;
 
-        if(decoded.role !== 'admin'){
-            return res.status(401).json({
-                messege:"You are not allowed to upload questions"
-            })
-        }
+    //Need to pass the ans as an array of Strings from the fontend
+    const { hints, ans } = req.body;
 
-        const file = req.file
+    const result = await uploadToCloud(file.buffer);
 
-        //Need to pass the ans as an array of Strings from the fontend
-        const {hints, ans} = req.body
+    const question = await picqsModel.create({
+      image: result.url,
+      hints,
+      ans,
+    });
 
-        const result = await uploadToCloud(file.buffer)
-
-        const question = await picqsModel.create({
-            image:result.url,
-            hints,
-            ans
-        })
-
-        res.status(200).json({
-            messege:"Questions is uploaded successfully",
-            question
-        })
-
-    }catch(err){
-        console.log(err)
-        return res.status(401).json({
-            messege:"Unauthorized"
-        })
-    }
+    res.status(200).json({
+      messege: "Questions is uploaded successfully",
+      question,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({
+      messege: "Unauthorized",
+    });
+  }
 }
 
-module.exports = {uploadMcqQuestions, uploadPictureQuestions}
+module.exports = { uploadMcqQuestions, uploadPictureQuestions };
--- backend/src/models/mcq.model.js
+++ backend/src/models/mcq.model.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const mcq = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    desc: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  opts: {
+    type: [String],
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
 
-const mcqModel = mongoose.model("quiz/mcq", mcq)
-module.exports = mcqModel
-
+const mcqModel = mongoose.model("mcq", mcq);
+module.exports = mcqModel;
