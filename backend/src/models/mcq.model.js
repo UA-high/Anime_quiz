06f48188ae04558c16c76d2962a7eed24de6e8d5
adityapaul26 <adityapaul2603@gmail.com>
2026-09-21 23:57:00 +0530

    mcq schema update

--- backend/src/models/mcq.model.js
+++ backend/src/models/mcq.model.js
@@ -1,31 +1,67 @@
 const mongoose = require("mongoose");
 
-const mcq = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-  },
-  desc: {
-    type: String,
-    required: [true, "Desc is required"],
-  },
-  ans: {
-    type: String,
-    required: [true, "Answer is required"],
-  },
-  opts: {
-    type: [String],
-    required: [true, "Options are required"],
-  },
-  image: {
-    type: String,
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "admin",
-    required: [true, "Author name is required"],
-  },
-});
+const mcqSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-const mcqModel = mongoose.model("mcq", mcq);
+    genres: {
+      type: [String],
+      required: true,
+    },
+
+    question: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    answer: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    options: {
+      type: [String],
+      required: true,
+      validate: {
+        validator: function (options) {
+          return options.length === 4;
+        },
+        message: "A question must have exactly 4 options",
+      },
+    },
+
+    hints: {
+      type: [String],
+      required: true,
+      validate: {
+        validator: function (hints) {
+          return hints.length === 2;
+        },
+        message: "A question must have exactly 2 hints",
+      },
+    },
+
+    explanation: {
+      type: String,
+      required: true,
+    },
+
+    difficulty: {
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const mcqModel = mongoose.model("mcq", mcqSchema);
 module.exports = mcqModel;
